a1ecc65fb4c7366c7300dcd5531586b9e5f13b24
sonia <sonia.sarni26@gmail.com>
2021-03-26 17:18:44 +0100

    chercher une valeur precise dans lr tableau

--- TableauMaxMin.java
+++ TableauMaxMin.java
@@ -26,6 +26,30 @@ public class TableauMaxMin {
 	}
 System.out.println("la valeur max du tableau est: "+max );
 System.out.println("la valeur main du tableau est: "+min);
+//On cherche à savoir si une  une valeur precise est presente dans notre tableau
+//combien fois cette que cette valeur elle y'est ?
+	
+System.out.println("entrez la valeur que vous cherchez");
+//Scanner scan= new Scanner(System.in);
+
+int count =0;
+int valeurRecherche= scan.nextInt();
+for (int i= 0; i< tab.length; i++) {
+	
+
+
+	if (valeurRecherche==tab[i]) {
+		count++;
+		
 	}
 
+	}
+if (count!=0) {
+System.out.println("la valeur " +valeurRecherche+" est bien presente dans le tableau");
+System.out.print(valeurRecherche +" est présente "+count++  +" fois dans le tableau");
+}else 
+System.out.println("la valeur " +valeurRecherche+" est introuvable dans le tableau");
+System.out.print(valeurRecherche +" est "+count++  +" fois dans le tableau");
+	}
 }
+
